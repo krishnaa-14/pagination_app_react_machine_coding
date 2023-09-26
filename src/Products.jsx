aa14136fe35f0ffa1dd9f836a3f5b0d13bf4991e
Krishna Vamsi Durga <krishnavamsidurga@Krishnas-Air.bbrouter>
2023-09-27 00:10:00 +0530

    Pagination Added - Completely on front end specific assuming we will get products all at a time, still there is scope for improvement by calling api's on limit

--- src/Products.jsx
+++ src/Products.jsx
@@ -6,6 +6,7 @@ import ProductCard from './ProductCard';
 const Products = () => {
 
     const [products, setProducts] = useState([]);
+    const [page, setPage] = useState(1);
 
     const getProducts = async () => {
         const data = await fetch("https://dummyjson.com/products?limit=100");
@@ -21,7 +22,7 @@ const Products = () => {
         <div style={{textAlign: "center", margin: "20px"}}>
             <h1>Products</h1>
             <div style={{display: "flex", justifyContent: "center", flexWrap: "wrap"}}>
-                {products.length > 0 && products.map((product) => (
+                {products.length > 0 && products.slice(page * 10 - 10, page * 10).map((product) => (
                     <ProductCard
                         key={product.id}
                         imglink={product.thumbnail}
@@ -31,6 +32,34 @@ const Products = () => {
                     />
                 ))}
             </div>
+
+
+            <div>
+                {
+                    products.length > 0 && 
+
+                    <div style = {{display : "flex", justifyContent : "center"}}>
+                        { page !== 1 && <div onClick = {() => setPage(page - 1)} style = {{cursor : "pointer", border : "1px solid black", padding : "25px", fontSize : "40px"}}>
+                            ◀️
+                        </div> }
+
+                        {
+                            [...Array(products.length /10)].map((_, index) => 
+                                <div onClick = {() => setPage(index + 1)} style = {{backgroundColor : (page === index + 1 ) ? "lightgray" : "white", cursor : "pointer", border : "1px solid black", padding : "25px", fontSize : "40px"}}>
+                                    {index}
+                                </div>
+                            )
+                        }
+
+                        { page !== products.length / 10 && <div onClick = {() => setPage(page + 1)} style = {{cursor : "pointer", border : "1px solid black", padding : "25px", fontSize : "40px"}}>
+                            ▶️
+                        </div> }
+                    </div>
+
+
+                }
+            </div>
+
         </div>
     )
 }
